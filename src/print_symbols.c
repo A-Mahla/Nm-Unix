ba9d20767eee52e7862f963c68f778c134acf484
A-Mahla <>
2023-11-05 22:37:02 +0100

    print_symbols.c : letter to print in progress

--- src/print_symbols.c
+++ src/print_symbols.c
@@ -6,7 +6,7 @@
 /*   By: amahla <ammah.connect@outlook.fr>       +#+  +:+    +#+     +#+      */
 /*                                             +#+    +#+   +#+     +#+       */
 /*   Created: 2023/11/05 02:37:58 by amahla  #+#      #+#  #+#     #+#        */
-/*   Updated: 2023/11/05 03:07:43 by amahla ###       ########     ########   */
+/*   Updated: 2023/11/05 22:35:45 by amahla ###       ########     ########   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,68 @@ void	which_type(unsigned char type);
 void	which_bind(unsigned char bind);
 void	which_other(unsigned char other);
 
+char	print_letter64(Elf64_Sym *sym, Elf64_Ehdr *ehdr);
+
 
 void	print_symbols(struct filedata_s *binary)
 {
+	
 	Elf64_Sym	**symtab = (Elf64_Sym **)binary->symtab;
 	
 	for	(size_t i= 0; symtab[i]; i++) {
-		ft_printf("%016x ", symtab[i]->st_value); 
+		if (symtab[i]->st_value)
+			ft_printf("%016x", symtab[i]->st_value); 
+		else
+			ft_printf("                ");
+		ft_printf(" %c ", print_letter64(symtab[i], (Elf64_Ehdr *)binary->file)); 
 		ft_printf("%s\n", binary->strtab + symtab[i]->st_name);
 	}
+}
 
+
+char	print_letter64(Elf64_Sym *sym, Elf64_Ehdr *ehdr)
+{
+	Elf64_Shdr	*sht = (Elf64_Shdr *)((uint8_t *)ehdr + ehdr->e_shoff);
+	char		c;
+
+	if (ELF64_ST_BIND(sym->st_info) == STB_GNU_UNIQUE) {
+		return 'u';
+	} else if (sym->st_shndx == SHN_ABS) {
+		return 'A';
+	} else if (ELF64_ST_BIND(sym->st_info) == STB_WEAK
+		&& ELF64_ST_TYPE(sym->st_info) == STT_OBJECT) {
+		if (sym->st_shndx == SHN_UNDEF)
+			return 'v';
+		return 'V';
+	} else if (ELF64_ST_BIND(sym->st_info) == STB_WEAK) {
+		if (sym->st_shndx == SHN_UNDEF)
+			return 'w';
+		return 'W';
+	} else if (sym->st_shndx == SHN_UNDEF) {
+		return 'U';
+	} else if (sht[sym->st_shndx].sh_type == SHT_NOBITS
+		&& sht[sym->st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE)) {
+		c = 'B';
+	} else if (sht[sym->st_shndx].sh_type == SHT_PROGBITS
+		&& sht[sym->st_shndx].sh_flags == SHF_ALLOC) {
+		c = 'R';
+	} else if (sht[sym->st_shndx].sh_type == SHT_PROGBITS
+		&& sht[sym->st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE)) {
+		c = 'D';
+	} else if (sht[sym->st_shndx].sh_type == SHT_PROGBITS
+		&& sht[sym->st_shndx].sh_flags == (SHF_ALLOC | SHF_EXECINSTR)) {
+		c = 'T';
+	} else if (sht[sym->st_shndx].sh_type == SHT_DYNAMIC) {
+		c = 'D';
+	} else if (ELF64_ST_TYPE(sym->st_info) == STT_COMMON) {
+		c = 'C';
+	} else {
+		return '?';
+	}
+	if (ELF64_ST_BIND(sym->st_info) == STB_LOCAL)
+		c += 32;
+	return c;
+	
 }
 
 
